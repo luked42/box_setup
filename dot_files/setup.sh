e67e5d271a31eabf5b61b0bb2d0665870a3337bf
luke <luke@localhost.localdomain>
2020-03-14 19:56:14 +0000

    fixing ccls cache dir

--- dot_files/setup.sh
+++ dot_files/setup.sh
@@ -1,5 +1,10 @@
 #!/bin/bash -ex
 
+PREFIX_DIR=$1
+if [[ -z $PREFIX_DIR ]]; then
+	PREFIX_DIR="${HOME}/.local"
+fi
+
 BASE_DIR=$(dirname ${0})
 
 # copy to home directory
@@ -9,4 +14,4 @@ done
 
 mkdir -p ${HOME}/.config/nvim
 cp ${BASE_DIR}/init.vim ${HOME}/.config/nvim/.
-cp ${BASE_DIR}/coc-settings.json ${HOME}/.config/nvim/.
+sed "s#CCLS_CACHE_DIR#${PREFIX_DIR}/ccls#" ${BASE_DIR}/coc-settings.json > ${HOME}/.config/nvim/coc-settings.json
