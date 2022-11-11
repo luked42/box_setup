407e14db03808200af55b2a4d957d11fc41392db
luked42 <lukedolan42@gmail.com>
2022-11-11 21:53:09 +0100

    Update setup.sh

--- dot_files/setup.sh
+++ dot_files/setup.sh
@@ -12,7 +12,7 @@ for dot_file in .bashrc .tmux.conf .clang-format; do
 	cp ${BASE_DIR}/${dot_file} ${HOME}/.
 done
 
-mkdir -p 4{HOME}/.config/.extra_dots
+mkdir -p 4{HOME}/.config/extra_dots
 mkdir -p ${HOME}/.config/nvim
 cp ${BASE_DIR}/init.vim ${HOME}/.config/nvim/.
 sed "s#CCLS_CACHE_DIR#${PREFIX_DIR}/ccls#" ${BASE_DIR}/coc-settings.json > ${HOME}/.config/nvim/coc-settings.json
